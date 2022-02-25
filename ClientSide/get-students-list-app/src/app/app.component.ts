97f916f713e41def5be1b75ee6e9c62ad2d29568
uzair.iqbal <uzair.iqbal@CMDLHRLT804>
2022-02-25 14:58:11 +0500

    Client side addition

---
+++ ClientSide/get-students-list-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'get-students-list-app';
+  data;
+constructor(private httpClient: HttpClient) {}
+  OnClick() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((res)=> {
+    this.data = res;
+    })
+  }
+
+  OnEraseButtonClick() {
+    this.data = null;
+  }
+}
